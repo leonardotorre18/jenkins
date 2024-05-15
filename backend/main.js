9e4c5ea3745c6e4daa5d51d126081ae39ee42cb1
Leonardotorre18 <Leonardotorre14@gmail.com>
2024-05-14 21:13:18 -0400

    PORT environment variable

--- backend/main.js
+++ backend/main.js
@@ -1,9 +1,11 @@
 const express = require('express')
 
+require('dotenv').config()
+
 const server = express()
 
 server.get('/', (req, res) => {
     res.status(200).json({ message: 'ok' })
 })
 
-server.listen(3000, () => console.log('listening'))
+server.listen(process.env.PORT, () => console.log('listening'))
